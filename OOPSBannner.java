0d75944b42af844cc6cc3f1cdc4b9a4c85a1614e
Shantanu1960 <sk1960@srmist.edu.in>
2026-02-19 18:29:05 +0530

    added code in development branch

--- OOPSBannner.java
+++ OOPSBannner.java
@@ -1,5 +1,5 @@
-class main{
-    public static void main(String[] args){
-        system.out.println("Hello World");
+public class OOPSBannner {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
     }
 }
