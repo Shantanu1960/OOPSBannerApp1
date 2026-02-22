d50c6eee96019f49945fe9d6fdbd78e44675f693
Shantanu1960 <sk1960@srmist.edu.in>
2026-02-22 15:13:13 +0530

    Coded UC2 to print OOPS ASCII Banner

--- OOPSBannner.java
+++ OOPSBannner.java
@@ -1,5 +1,31 @@
 public class OOPSBannner {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Line 1 - Top borders of all letters
+        String line1 = "  *****   " + "  *****   " + "****** " + "   *******  ";
+        System.out.println(line1);
+        
+        // Line 2
+        String line2 = " *     *  " + " *     *  " + "**    **" + " **      ";
+        System.out.println(line2);
+        
+        // Line 3
+        String line3 = "*       * " + "*       * " + "**    **" + " **        ";
+        System.out.println(line3);
+        
+        // Line 4 - Middle of letters
+        String line4 = "*       * " + "*       * " + "****** " + "   ******* ";
+        System.out.println(line4);
+        
+        // Line 5
+        String line5 = "*       * " + "*       * " + "**        " + "       ** ";
+        System.out.println(line5);
+        
+        // Line 6
+        String line6 = " *     *  " + " *     *  " + "**        " + "       ** ";
+        System.out.println(line6);
+        
+        // Line 7 - Bottom borders of all letters
+        String line7 = "  *****   " + "  *****   " + "**        " + " ******* ";
+        System.out.println(line7);
     }
 }
