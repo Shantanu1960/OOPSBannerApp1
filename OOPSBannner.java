95f8b3c6edef65ec2212d1838b6380f8945b16a5
Shantanu1960 <sk1960@srmist.edu.in>
2026-02-22 19:52:21 +0530

    Coded UC4 using String Array and Enhanced For Loop

--- OOPSBannner.java
+++ OOPSBannner.java
@@ -1,31 +1,20 @@
 public class OOPSBannner {
     public static void main(String[] args) {
-       
-        String line1 = String.join("", "  *****   ", "  *****   ", "****** ", "   *******  ");
-        System.out.println(line1);
+        // UC4: String Array to store all lines of the OOPS banner
+        String[] bannerLines = new String[7];
         
+        // Populate array with each line using String.join()
+        bannerLines[0] = String.join("", "  *****   ", "  *****   ", "****** ", "   *******  ");
+        bannerLines[1] = String.join("", " *     *  ", " *     *  ", "**    **", " **      ");
+        bannerLines[2] = String.join("", "*       * ", "*       * ", "**    **", " **        ");
+        bannerLines[3] = String.join("", "*       * ", "*       * ", "****** ", "   ******* ");
+        bannerLines[4] = String.join("", "*       * ", "*       * ", "**        ", "       ** ");
+        bannerLines[5] = String.join("", " *     *  ", " *     *  ", "**        ", "       ** ");
+        bannerLines[6] = String.join("", "  *****   ", "  *****   ", "**        ", " ******* ");
         
-        String line2 = String.join("", " *     *  ", " *     *  ", "**    **", " **      ");
-        System.out.println(line2);
-        
-       
-        String line3 = String.join("", "*       * ", "*       * ", "**    **", " **        ");
-        System.out.println(line3);
-        
-        // Line 4 - Middle of letters
-        String line4 = String.join("", "*       * ", "*       * ", "****** ", "   ******* ");
-        System.out.println(line4);
-        
-        // Line 5
-        String line5 = String.join("", "*       * ", "*       * ", "**        ", "       ** ");
-        System.out.println(line5);
-        
-        // Line 6
-        String line6 = String.join("", " *     *  ", " *     *  ", "**        ", "       ** ");
-        System.out.println(line6);
-        
-        // Line 7 - Bottom borders of all letters
-        String line7 = String.join("", "  *****   ", "  *****   ", "**        ", " ******* ");
-        System.out.println(line7);
+        // Enhanced for-each loop to iterate through array and print each line
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
     }
 }
