c76506bf976a50bf9ba731868b5849708c3f0755
Shantanu1960 <sk1960@srmist.edu.in>
2026-02-22 19:35:50 +0530

    coded UC3 to implement string.join()

--- OOPSBannner.java
+++ OOPSBannner.java
@@ -1,31 +1,31 @@
 public class OOPSBannner {
     public static void main(String[] args) {
-        // Line 1 - Top borders of all letters
-        String line1 = "  *****   " + "  *****   " + "****** " + "   *******  ";
+       
+        String line1 = String.join("", "  *****   ", "  *****   ", "****** ", "   *******  ");
         System.out.println(line1);
         
-        // Line 2
-        String line2 = " *     *  " + " *     *  " + "**    **" + " **      ";
+        
+        String line2 = String.join("", " *     *  ", " *     *  ", "**    **", " **      ");
         System.out.println(line2);
         
-        // Line 3
-        String line3 = "*       * " + "*       * " + "**    **" + " **        ";
+       
+        String line3 = String.join("", "*       * ", "*       * ", "**    **", " **        ");
         System.out.println(line3);
         
         // Line 4 - Middle of letters
-        String line4 = "*       * " + "*       * " + "****** " + "   ******* ";
+        String line4 = String.join("", "*       * ", "*       * ", "****** ", "   ******* ");
         System.out.println(line4);
         
         // Line 5
-        String line5 = "*       * " + "*       * " + "**        " + "       ** ";
+        String line5 = String.join("", "*       * ", "*       * ", "**        ", "       ** ");
         System.out.println(line5);
         
         // Line 6
-        String line6 = " *     *  " + " *     *  " + "**        " + "       ** ";
+        String line6 = String.join("", " *     *  ", " *     *  ", "**        ", "       ** ");
         System.out.println(line6);
         
         // Line 7 - Bottom borders of all letters
-        String line7 = "  *****   " + "  *****   " + "**        " + " ******* ";
+        String line7 = String.join("", "  *****   ", "  *****   ", "**        ", " ******* ");
         System.out.println(line7);
     }
 }
