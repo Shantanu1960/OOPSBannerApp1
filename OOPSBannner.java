5ec1e117138ae2b294247410d71153e15bb2855e
Shantanu1960 <sk1960@srmist.edu.in>
2026-02-27 15:17:36 +0530

    Implemented for loop

--- OOPSBannner.java
+++ OOPSBannner.java
@@ -1,16 +1,15 @@
 public class OOPSBannner {
     public static void main(String[] args) {
-        // UC4: String Array to store all lines of the OOPS banner
-        String[] bannerLines = new String[7];
-        
-        // Populate array with each line using String.join()
-        bannerLines[0] = String.join("", "  *****   ", "  *****   ", "****** ", "   *******  ");
-        bannerLines[1] = String.join("", " *     *  ", " *     *  ", "**    **", " **      ");
-        bannerLines[2] = String.join("", "*       * ", "*       * ", "**    **", " **        ");
-        bannerLines[3] = String.join("", "*       * ", "*       * ", "****** ", "   ******* ");
-        bannerLines[4] = String.join("", "*       * ", "*       * ", "**        ", "       ** ");
-        bannerLines[5] = String.join("", " *     *  ", " *     *  ", "**        ", "       ** ");
-        bannerLines[6] = String.join("", "  *****   ", "  *****   ", "**        ", " ******* ");
+        // UC5: Inline array initialization using String.join() calls
+        String[] bannerLines = new String[] {
+            String.join("", "  *****   ", "  *****   ", "****** ", "   *******  "),
+            String.join("", " *     *  ", " *     *  ", "**    **", " **      "),
+            String.join("", "*       * ", "*       * ", "**    **", " **        "),
+            String.join("", "*       * ", "*       * ", "****** ", "   ******* "),
+            String.join("", "*       * ", "*       * ", "**        ", "       ** "),
+            String.join("", " *     *  ", " *     *  ", "**        ", "       ** "),
+            String.join("", "  *****   ", "  *****   ", "**        ", " ******* ")
+        };
         
         // Enhanced for-each loop to iterate through array and print each line
         for (String line : bannerLines) {
